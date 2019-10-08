fde44613ad52a208ebe7b27b06570894233c195b
tothricsaj <tothricsaj@gmail.com>
2019-10-08 16:41:11 +0200

    no working ------- yet

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
 
-module.exports = merge({
+module.exports = merge(common, {
     mode: "production",
     output: {
         filename: "main.[contentHash].js",
