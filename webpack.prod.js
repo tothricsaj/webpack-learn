a43724117be1c29c5904686283caaa9660917e21
tothricsaj <tothricsaj@gmail.com>
2019-10-11 06:31:18 +0200

    solved build bug -- packages was the solution

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
-const CleanWebpackPlugin = require();
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
     output: {
-        filename: "main.[contentHash].js",
+        filename: "[name].[contentHash].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
     plugins: [new CleanWebpackPlugin()]
 });
+
+
